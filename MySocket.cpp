cac09a7540a4326c2d81fd40d228675dc147577c
janetchu05 <82367137+janetchu05@users.noreply.github.com>
2021-07-15 10:39:47 +0900

    Add files via upload

---
+++ MySocket.cpp
@@ -0,0 +1,44 @@
+﻿// MySocket.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ProjectMFC.h"
+#include "MySocket.h"
+#include "ProjectMFCDlg.h"  //***다이얼로그 클래스를 포함해서 보내기/받기 함수만들예정
+
+
+// CMySocket
+
+CMySocket::CMySocket()
+{
+}
+
+CMySocket::~CMySocket()
+{
+}
+
+
+// CMySocket 멤버 함수
+void CMySocket::OnAccept(int nErrorCode)
+{
+	if (nErrorCode == 0)
+		((CProjectMFCDlg*)m_pWnd)->OnAccept();
+}
+
+void CMySocket::OnReceive(int nErrorCode)
+{
+	if (nErrorCode == 0)
+		((CProjectMFCDlg*)m_pWnd)->OnReceive();
+}
+
+void CMySocket::OnSend(int nErrorCode)
+{
+	if (nErrorCode == 0)
+		((CProjectMFCDlg*)m_pWnd)->OnSend();
+}
+
+void CMySocket::OnConnect(int nErrorCode)
+{
+	if (nErrorCode == 0)
+		((CProjectMFCDlg*)m_pWnd)->OnConnect();
+}
---
+++ ProjectMFCDlg.h
@@ -0,0 +1,58 @@
+﻿
+// ProjectMFCDlg.h: 헤더 파일
+//
+#include "MySocket.h"
+
+#pragma once
+
+
+// CProjectMFCDlg 대화 상자
+class CProjectMFCDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CProjectMFCDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+	void OnAccept();
+	void OnReceive();
+	void OnSend();
+	void OnConnect();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PROJECTMFC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedBtnSend();
+	afx_msg void OnBnClickedBtnListen();
+	afx_msg void OnBnClickedBtnConnect();
+	CListBox m_ctlRecv;
+	CListBox m_ctlList;
+	CString m_strMessage;
+
+private:
+	CMySocket m_sListenSocket;
+	CMySocket m_sConnectSocket;
+	BOOL m_bConnected;
+public:
+	CIPAddressCtrl m_ipControl;
+	CString strName; //닉네임 edit control에서 값 받아서 넣을 변수 --현재 창 닉네임
+	CString badWords[2] = { "심한욕", "나쁜말" };
+	int badLen = sizeof(badWords) / sizeof(badWords[0]);
+};
